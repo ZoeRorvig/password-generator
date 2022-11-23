cd75791b5bfb5b49d72c30768de4493068076e36
Zoe Rorvig <zrorvig@gmail.com>
2022-11-23 18:14:23 -0500

    added password length input

--- script.js
+++ script.js
@@ -4,6 +4,27 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword() {
 // Reset Password
 password = "";
+
+// Declaring variables
+var passwordLength = 0;
+
+// Inputting password length
+while(passwordLength < 8 || passwordLength > 128){
+  
+  passwordLength = prompt("Please enter the length of your password (must be at least 8 characters and no more than 128 characters):");
+
+//Checking if what the user entered is a number?
+  /*if(typeof passwordLength != "number"){
+    alert("Please enter a number!");
+    passwordLength = 0;
+  }*/ 
+
+  if(passwordLength < 8){
+    alert("Password must be greater than 8 characters.");
+  }else if(passwordLength > 128){
+    alert("Password must be less than 128 characters.");
+  }
+}
   
   return password;
 }
