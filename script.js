99b969bafac0d3a01342cb9c3740fdc7a533e748
Zoe Rorvig <zrorvig@gmail.com>
2022-11-29 15:29:46 -0500

    updated password length section - check for null or non-numbers

--- script.js
+++ script.js
@@ -2,81 +2,94 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-// Reset Password
-password = "";
+  // Reset Password
+  password = "";
 
-// Declaring variables
-var passwordLength = 0;
-var passwordOptions = "";
-var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
-var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numberChoices = "0123456789";
-var specialCharacters ="!#$%&()*+,-./:;<=>?@[\]^_`{|}~";
-var userChoice = false;
-var randomPick = "";
+  // Declaring variables
+  var passwordLength = 0;
+  var passwordOptions = "";
+  var randomPick = "";
+  var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseLetters = lowercaseLetters.toUpperCase();
+  var numbers = "0123456789";
+  var specialCharacters = "!#$%&()*+,-./:;<=>?@[\]^_`{|}~";
+  var userChoice = false;
 
-// Inputting password length
-while(passwordLength < 8 || passwordLength > 128){
-  
+
+  //TODO: must pick at least one character.
+
+  //Get the password length.
   passwordLength = prompt("How many characters would you like your password to contain?");
+  console.log(passwordLength);
 
-//Checking if what the user entered is a number?
-  /*if(typeof passwordLength != "number"){
-    alert("Please enter a number!");
-    passwordLength = 0;
-  }*/ 
+  if(passwordLength===null) return passwordLength;
 
-  if(passwordLength < 8){
+  passwordLength = parseInt(passwordLength);
+
+  if(isNaN(passwordLength)) {
+    alert("Password must be a valid number.");
+    return generatePassword();
+  }
+  else if(passwordLength < 8) {
     alert("Password must be at least 8 characters.");
-  }else if(passwordLength > 128){
+    
+    return generatePassword();
+  } else if(passwordLength > 128) {
     alert("Password must be less than 128 characters.");
+    return generatePassword();
+  }
+  //}
+
+  //Include Special Characters?
+  userChoice = confirm("Click OK to confirm including special characters.");
+  if(userChoice == true) {
+    passwordOptions += specialCharacters;
+  }
+
+  console.log(passwordOptions);
+
+  //Include Numeric?
+  userChoice = confirm("Click OK to confirm including numeric characters.");
+  if(userChoice == true) {
+    passwordOptions += numbers;
+  }
+
+  console.log(passwordOptions);
+
+  //Include Lowercase?
+  userChoice = confirm("Click OK to confirm including lowercase characters.");
+  if(userChoice == true) {
+    passwordOptions += lowercaseLetters;
+  }
+
+  console.log(passwordOptions);
+
+  //Include Uppercase?
+  userChoice = confirm("Click OK to confirm including uppercase characters.");
+  if(userChoice == true) {
+    passwordOptions += uppercaseLetters;
+  }
+
+  console.log(passwordOptions);
+
+  //Check to make sure characters were selected.
+  //Generate Password
+  if(passwordOptions === ""){
+    password = "No Characters Selected";
+    console.log(password);
+  }else{
+    for(var i = 0; i < passwordLength; i++) {
+      randomPick = Math.floor(Math.random() * passwordOptions.length);
+      password += passwordOptions[randomPick];
   }
 }
-
-//Include Special Characters?
-userChoice = confirm("Click OK to confirm including special characters.");
-if(userChoice == true){
-  passwordOptions += specialCharacters;
-}
-
-console.log(passwordOptions);
-
-//Include Numeric?
-userChoice = confirm("Click OK to confirm including numeric characters.");
-if(userChoice == true){
-  passwordOptions += numberChoices;
-}
-
-console.log(passwordOptions);
-
-//Include Lowercase?
-userChoice = confirm("Click OK to confirm including lowercase characters.");
-if(userChoice == true){
-  passwordOptions += lowercaseLetters;
-}
-
-console.log(passwordOptions);
-
-//Include Uppercase?
-userChoice = confirm("Click OK to confirm including uppercase characters.");
-if(userChoice == true){
-  passwordOptions += uppercaseLetters;
-}
-
-console.log(passwordOptions);
-
-//Generate Password
-for(var i = 0; i < passwordLength; i++){
-  randomPick = Math.floor(Math.random() * passwordOptions.length);
-  password += passwordOptions.substring(randomPick, randomPick +1);
-}
-  
   return password;
 }
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+  if(!password) return;
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
